cae789a3aa5c51f576a52a79cbdaeedf31a07dab
Redzzaja <maulanaarif2707@gmail.com>
2025-11-05 11:41:01 +0700

    tambahin

---
+++ TubesPBO/src/tubespbo/TubesPBO.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tubespbo;
+
+/**
+ *
+ * @author redzz
+ */
+public class TubesPBO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new LoginPage().setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
